ad18ab52fd61e0616fe1511d3a4d0f0b5943014b
Redfire <redfire75369@hotmail.com>
2022-11-12 22:05:52 +0800

    Added Social Media Links
    Fixed Site Logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,11 +15,17 @@ function Header(props: HeaderProps) {
 			<div className={header}>
 				<div className={headerInner}>
 					<a href="/">
-						<img src="/img/icon.png" alt="Silicon Logo"/>
+						<img src="/images/icon.png" alt="Silicon Logo"/>
 					</a>
 					<div className={title}>
 						<h1>{props.title}</h1>
 					</div>
+					<a href="https://twitter.com/Redfire75369">
+						<img src="images/twitter.png" alt="Twitter"/>
+					</a>
+					<a href="https://mastodon.online/@redfire" rel="me">
+						<img src="images/mastodon.png" alt="Mastodon"/>
+					</a>
 				</div>
 				<hr/>
 			</div>
